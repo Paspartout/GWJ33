e47438877e9ec148cb424feb5aee9052141335ec
Paspartout <2720369+Paspartout@users.noreply.github.com>
2021-05-15 21:02:10 +0200

    Add initial grappling hook

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,8 +8,11 @@ export var decelleartion: float = 0.85
 export var gravity: float = 10
 export var jump_strength: float = 300
 
+onready var grapple: GrapplingHook = $Items/GrapplingHook
+export var grapple_indicator_path: NodePath
+
 func _ready():
-	pass # Replace with function body.
+	grapple.grapple_indicator = get_node(grapple_indicator_path)
 
 func _input(event):
 	if is_on_floor():
@@ -19,6 +22,10 @@ func _input(event):
 		if event.is_action_released("jump") and velocity.y < -jump_strength/2:
 			velocity.y = -jump_strength/2
 
+func _process(delta):
+	# TODO: Rework for controller input
+	pass
+
 func _physics_process(delta):
 	var input_x = Input.get_action_strength("right") - Input.get_action_strength("left")
 	
@@ -26,9 +33,13 @@ func _physics_process(delta):
 		velocity.x += acceleration * input_x
 	else:
 		velocity.x *= decelleartion
-	
+
+	# TODO: More generic item system
+	velocity += grapple.pull()
+
 	velocity.x = clamp(velocity.x, -max_speed, max_speed)
-	
+	velocity.y = clamp(velocity.y, -max_speed, max_speed)
+
 	if is_on_floor():
 		pass
 	else:
@@ -36,5 +47,6 @@ func _physics_process(delta):
 		
 	if is_on_ceiling():
 		velocity.y = 0
+	
 	
 	move_and_slide(velocity, Vector2.UP)
