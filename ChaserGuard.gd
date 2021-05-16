5144776ca8c31f3f57f37936028c96596fb73e2c
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-05-16 15:19:21 +0100

    Fixed enemy gravity and added collision layers

--- ChaserGuard.gd
+++ ChaserGuard.gd
@@ -1,21 +1,19 @@
 extends KinematicBody2D
 
-var run_speed = 100
+export var run_speed = 70
 var velocity = Vector2.ZERO
 var player = null
-var gravity =  10
+export var gravity =  30
 
 func _physics_process(delta):
-	velocity.y += gravity
-	
-	velocity = Vector2.ZERO
 	if player:
-		velocity = global_position.direction_to(player.global_position) * run_speed
+		var player_direction = global_position.direction_to(player.global_position) * run_speed
+		velocity.x = player_direction.x
+	velocity.y += gravity
 	velocity = move_and_slide(velocity)
-
 func _on_Detection_body_entered(body):
-	if body.get_name() == "Player":
-		player = body
+	player = body
 
 func _on_Detection_body_exited(body):
 	player = null
+	velocity = Vector2.ZERO
