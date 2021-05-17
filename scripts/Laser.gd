20d241e79dfa1c603ec0a0812c54ca275aad5ea4
Sebastian <sebastian.gallard.13@sansano.usm.cl>
2021-05-17 11:26:04 -0400

    fix commit issue with Laser scene

---
+++ scripts/Laser.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const MAX_LENGTH = 2080
+
+onready var beam = $Beam
+onready var end = $End
+onready var raycast = $RayCast2D
+
+func _physics_process(delta):
+	var mouse_position = get_local_mouse_position()
+	var max_cast_to = mouse_position.normalized() * MAX_LENGTH
+	raycast.cast_to = max_cast_to
+	if raycast.is_colliding():
+		end.global_position = raycast.get_collision_point()
+	else:
+		end.global_position = raycast.cast_to
+	beam.rotation = raycast.cast_to.angle()
+	beam.region_rect.end.x = end.position.length()
