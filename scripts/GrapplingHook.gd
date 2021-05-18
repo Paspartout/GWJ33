65d52381c806b08b9c6ed510dc26f2a6845f9f7f
Paspartout <2720369+Paspartout@users.noreply.github.com>
2021-05-18 21:27:10 +0200

    nerf grapple distance, add croshair for grapple direction

--- scripts/GrapplingHook.gd
+++ scripts/GrapplingHook.gd
@@ -2,9 +2,11 @@ class_name GrapplingHook
 
 extends Node2D
 
+const CROSHAIR: Texture = preload("res://graphics/crosshair.png")
+
 export var player_path: NodePath
 export var grapple_indicator_path: NodePath
-export var max_grapple_distance: float = 1000
+export var max_grapple_distance: float = 350
 export var grapple_acceleration: float = 100
 export var grapple_speed: float = 50
 
@@ -12,6 +14,10 @@ enum GrappleState {Loose, Shooting, Hooked, Pulling}
 var grapple_state = GrappleState.Loose
 var grapple_pos: Vector2
 var grapple_velocity: Vector2 = Vector2.ZERO
+var grapple_direction: Vector2 = Vector2.ZERO
+
+var grapple_indicator: Node2D
+var direction_indicator: Node2D
 
 onready var player = get_node(player_path)
 
@@ -19,21 +25,47 @@ onready var grapple_timer: Timer = $Timer
 onready var grapple_cast: RayCast2D = $GrappleRayCast
 onready var rope: Line2D = $Rope
 
-var grapple_indicator: Node2D
 
 func _ready():
 	grapple_timer.connect("timeout", self, "stop", [], CONNECT_ONESHOT)
 
-	# Setup temporary grapple indicator
 	grapple_indicator = Sprite.new()
-	grapple_indicator.scale = Vector2(0.2, 0.2)
-	grapple_indicator.texture = preload("res://icon.png")
+	grapple_indicator.hframes = 2
+	grapple_indicator.frame = 0
+	grapple_indicator.texture = CROSHAIR
 	player.get_parent().call_deferred("add_child", grapple_indicator)
+	
+	direction_indicator = Sprite.new()
+	direction_indicator.hframes = 2
+	direction_indicator.frame = 0
+	direction_indicator.texture = CROSHAIR
+	call_deferred("add_child", direction_indicator)
 
 func _input(event):
 	# TODO: Maybe move somewhere different
 	if event.is_action_pressed("graple"):
 		shoot()
+
+
+func _physics_process(_delta):
+	
+	var input_vec = Vector2(
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	
+	if input_vec != Vector2.ZERO:
+		grapple_direction = input_vec.normalized()
+
+	grapple_cast.cast_to = grapple_direction * max_grapple_distance
+	direction_indicator.position = grapple_direction.normalized() * 20
+	
+	if grapple_cast.is_colliding():
+		direction_indicator.frame = 1
+		grapple_indicator.frame = 1
+	else:
+		direction_indicator.frame = 0
+		grapple_indicator.frame = 0
 
 func pull() -> Vector2:
 	match grapple_state:
@@ -58,23 +90,10 @@ func shoot():
 	if grapple_state == GrappleState.Pulling:
 		stop()
 		return
-	var mouse_pos = get_global_mouse_position()
-	#var direction = (mouse_pos - player.position).normalized() * max_grapple_distance
-	var direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"),
-		Input.get_action_strength("down") - Input.get_action_strength("up")
-	).normalized() * max_grapple_distance
-	
-	#print("Mouse pos", mouse_pos)
-	#print("Grapple at", direction)
-	grapple_cast.cast_to = direction
-	grapple_cast.force_raycast_update()
 
 	if grapple_cast.is_colliding():
 		grapple_pos = grapple_cast.get_collision_point()
-		print("Grapple pos", grapple_pos)
 		
-
 		#print("Hit", grapple_pos)
 		grapple_indicator.global_position = grapple_pos
 		grapple_indicator.show()
