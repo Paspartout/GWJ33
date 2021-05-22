98336914fc4df332ad6d3ade399edb8d41721769
Paspartout <2720369+Paspartout@users.noreply.github.com>
2021-05-22 23:13:02 +0200

    Add Level switching

--- scenes/1 - 2.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-
-func _on_Area2D_body_entered(body):
-	pass
-	#get_tree().change_scene("res://scenes/Level2.tscn")
---
+++ scenes/levels/Level0_Tutorial.gd
@@ -0,0 +1,4 @@
+extends Level
+
+func _on_End_body_entered(body):
+	game.load_next_level()
