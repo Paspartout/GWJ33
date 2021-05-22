d4a25f780bed852ccc1cad809fc30582c0a2bb97
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-05-22 23:32:27 +0100

    finished main menu with options

---
+++ scenes/MainMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+onready var options = $Options
+onready var main_menu = $CenterContainer/MenuPanel
+
+func _on_Play_pressed() -> void:
+	get_tree().change_scene("res://Scenes/Game.tscn")
+
+func _on_Exit_pressed() -> void:
+	get_tree().quit()
+
+func _on_Options_pressed():
+	options.show()
+	main_menu.hide()
