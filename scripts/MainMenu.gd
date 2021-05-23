8799a23158aaf58cf80f9e4b1ea029171d00a1de
Sebastian <sebastian.gallard.13@sansano.usm.cl>
2021-05-23 00:30:14 -0400

    deleted some duplicated files

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -1,20 +1,18 @@
 extends Control
 
-func _on_Play_pressed():
+onready var options = $Options
+onready var main_menu = $CenterContainer/MenuPanel
+
+func _on_Play_pressed() -> void:
 	$Sounds/OptionSelect.play(1.11)
 	get_tree().change_scene("res://scenes/game.tscn")
-	
 
-
-
-func _on_Exit_pressed():
+func _on_Exit_pressed() -> void:
 	$Sounds/OptionSelect.play(1.11)
 	yield($Sounds/OptionSelect, "finished")
 	get_tree().quit()
 
-
 func _on_Options_pressed():
+	options.show()
+	main_menu.hide()
 	$Sounds/OptionSelect.play(1.11)
-	yield($Sounds/OptionSelect, "finished")
-	get_tree().change_scene("res://Options.tscn")
-
