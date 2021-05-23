49816e68b3d08551c83eb43d8061e94a15900c83
drkdragonz <57596304+drkdragonz@users.noreply.github.com>
2021-05-23 19:26:47 +0100

    Main Menu Makeover

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -3,8 +3,6 @@ extends Control
 onready var options = $Options
 onready var main_menu = $CenterContainer/MenuPanel
 
-func _ready():
-	$CPUParticles2D.emitting = true
 func _on_Play_pressed() -> void:
 	$Sounds/OptionSelect.play(1.11)
 	get_tree().change_scene("res://scenes/game.tscn")
@@ -18,3 +16,7 @@ func _on_Options_pressed():
 	options.show()
 	main_menu.hide()
 	$Sounds/OptionSelect.play(1.11)
+
+
+func _on_Timer_timeout():
+	$CPUParticles2D.emitting = true
